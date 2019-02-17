a146754b951fd811c185e32fdf65586c07c6174a
BAGNB1105\Björn Lundström <bjoern.lundstroem@brockhaus-ag.de>
2019-02-17 12:25:49 +0100

    add Example

---
+++ Blun.MQ.Example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Blun.MQ.Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Blun.MQ/Hosting/Host.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Blun.MQ.Abstractions;
+
+namespace Blun.MQ.Hosting
+{
+    public class Host : IAsyncDisposable
+    {   
+
+
+        public Task DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
